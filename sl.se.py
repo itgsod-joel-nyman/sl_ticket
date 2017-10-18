d3a00d2d06393c1805f8e1162752f15b1d4504ed
itgsod-joel-nyman <joel.nyman@elev.it-gymnasiet.se>
2017-10-18 14:11:17 +0200

    sl_ticket joel nyman

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,16 @@
 def fares(age, student=False, senior=False):
     print age,student,senior
-    return 'hel'
+    if age <=20:
+        return 'hel'
+    elif age >= 65:
+        return 'halv'
+    else:
+         return 'hel'
 
 
 
 
-
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+print 'barn 11', fares (11)
+print 'pension 65',fares (63)
+print 'student 22',fares (22)
+print 'vuxen 45', fares (45)
